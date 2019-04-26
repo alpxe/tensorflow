d8ec0dcacb49b2317a4b0ce7c9114982989c9fc0
Alpxe <alpxe@qq.com>
2019-04-26 16:46:02 +0800

    download修改

--- mnist/download.py
+++ mnist/download.py
@@ -1,35 +1,42 @@
-from six.moves import urllib
-from tensorflow.python.platform import gfile
 import os
+import urllib.request
+import sys
 
-# CVDF mirror of http://yann.lecun.com/exdb/mnist/
-DEFAULT_SOURCE_URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/'
+
+def maybe_download(file):
+    filepath = os.path.join(dest_directory, file)
+    urlpath = os.path.join(DEFAULT_SOURCE_URL, file)
+
+    if not os.path.exists(filepath):  # 如果文件夹中不存在该文件
+        # 动态的打印下载进度
+        def _progress(count, block_size, total_size):
+            sys.stdout.write(
+                '\r>> Downloading %s %.1f%%' % (file, float(count * block_size) / float(total_size) * 100.0))
+            sys.stdout.flush()
+            pass
+
+        # 下载文件
+        respath, _ = urllib.request.urlretrieve(urlpath, filepath, _progress)
+        statinfo = os.stat(respath)  # 获取文件属性信息
+        print('Successfully downloaded', file, statinfo.st_size, 'bytes.')
+        pass
+    pass
+
+
+dest_directory = "MNIST_DATA/"
+DEFAULT_SOURCE_URL = "https://storage.googleapis.com/cvdf-datasets/mnist/"
 
 TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
 TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
 TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
 TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
 
+if not os.path.exists(dest_directory):  # 如果文件夹不存在
+    os.makedirs(dest_directory)  # 创建文件夹
+    pass
 
-def urlretrieve_with_retry(url, filename=None):
-    return urllib.request.urlretrieve(url, filename)
-
-
-def maybe_download(filename, work_directory, source_url):
-    if not gfile.Exists(work_directory):
-        gfile.MakeDirs(work_directory)
-    filepath = os.path.join(work_directory, filename)
-    if not gfile.Exists(filepath):
-        temp_file_name, _ = urlretrieve_with_retry(source_url)
-        gfile.Copy(temp_file_name, filepath)
-        with gfile.GFile(filepath) as f:
-            size = f.size()
-        print('Successfully downloaded', filename, size, 'bytes.')
-    return filepath
-
-
-# 下载文件
-maybe_download(TRAIN_IMAGES, "MNIST_data/", DEFAULT_SOURCE_URL + TRAIN_IMAGES)
-maybe_download(TRAIN_LABELS, "MNIST_data/", DEFAULT_SOURCE_URL + TRAIN_LABELS)
-maybe_download(TEST_IMAGES, "MNIST_data/", DEFAULT_SOURCE_URL + TEST_IMAGES)
-maybe_download(TEST_LABELS, "MNIST_data/", DEFAULT_SOURCE_URL + TEST_LABELS)
+# 下载
+maybe_download(TRAIN_IMAGES)
+maybe_download(TRAIN_LABELS)
+maybe_download(TEST_IMAGES)
+maybe_download(TEST_LABELS)
