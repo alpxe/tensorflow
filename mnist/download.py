280202a09b080c6da088a9097cc8271e7ad3d593
Alpxe <alpxe@qq.com>
2019-04-26 17:43:33 +0800

    Update download.py

--- mnist/download.py
+++ mnist/download.py
@@ -23,10 +23,8 @@ def maybe_download(file):
     pass
 
 
-
-
 dest_directory = "MNIST_DATA/"
-DEFAULT_SOURCE_URL = "http://yann.lecun.com/exdb/mnist/" #mnist官方地址
+DEFAULT_SOURCE_URL = "http://yann.lecun.com/exdb/mnist/"  # mnist官方地址
 
 TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
 TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
